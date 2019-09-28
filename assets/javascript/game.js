33e5a032cdd37e9a7d5d94080f36caaea9713235
Nancy Harris <nanzyharris@gmail.com>
2019-09-28 02:27:36 -0500

    game js file updated but not complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@
 // ========================================================================
 // Arrays and variables for holding data
 
-var wordOptions = ["mustang", "continental", "marker", "daisy", "parabellum", "bourbon", "boogeyman"];
+var wordOptions = ["mustang", "continental", "marker", "daisy", "parabellum", "bourbon", "boogeyman", "jonathan"];
 var selectedWord = "";
 var lettersinWord = [];
 var numBlanks = 0;
@@ -32,8 +32,15 @@ function startGame() {
     // Populate blanks and successes with the right number of blanks
 
     for (var i = 0; i < numBlanks; i++) {
-        blanksAndSuccesses.push("");
+        blanksAndSuccesses.push(" ");
     }
+
+    // Change HTML to reflect game round conditions
+    document.getElementById("wordToGuess").innerHTML = blanksAndSuccesses.join(" ");
+    document.getElementById("numGuesses").innerHTML = guessesLeft;
+    document.getElementById("winCounter").innerHTML = winCount;
+    document.getElementById("lossCounter").innerHTML = lossCount;
+
     // Testing / Debugging
 
     console.log(selectedWord);
@@ -41,9 +48,68 @@ function startGame() {
     console.log(blanksAndSuccesses);
 }
 
+function checkLetters(letter) {
+
+    for (var i = 0; i < numBlanks; i++) {
+        if (selectedWord[i] == letter) {
+            isLetterInWord = true;
+        }
+    }
+    //Check where in word the letter exists
+    for (var i = 0; i < numBlanks; i++) {
+        if (selectedWord[i] == letter) {
+            blanksAndSuccesses[i] = letter;
+        }
+        else {
+            wrongLetters.push(letter);
+            guessesLeft--
+        }
+
+        // Testing and Debuggin
+        console.log(blanksAndSuccesses);
+    }
+}
+
+
+function roundComplete() {
+    console.log("Win Count: " + winCount + " | Loss Count: " + lossCount + " Guesses Left " + guessesLeft);
+
+    // Update the HTML to reflect the most recent count stats
+    document.getElementById("wordToGuess").innerHTML = blanksAndSuccesses.join(" ");
+    document.getElementById("numGuesses").innerHTML = guessesLeft;
+    document.getElementById("wrongGuess").innerHTML = wrongLetters.join(" ");
+
+    // Check if user won
+    if (isLetterInWord.toString() == blanksAndSuccesses.toString()) {
+        winCount++;
+        alert("You Won!")
+
+        // Update the win counter in the HTML
+        document.getElementById("winCounter").innerHTML = winCount;
+
+        startGame();
+    }
+    // Check if user lost
+    else if (guessesLeft == 0) {
+        lossCount++;
+        alert("You lost!");
+        // Update the HTML 
+        document.getElementById("lossCounter").innerHTML = lossCount;
+
+        startGame();
+    }
+}
+
 
 // MAIN PROCESS 
 // ========================================================================
 
-startGame();
+// Register keyclicks
 
+document.onkeyup = function (event) {
+    var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+    checkLetters(letterGuessed);
+    roundComplete();
+
+    console.log(letterGuessed);
+}
