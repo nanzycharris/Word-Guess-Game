f2cf6d7995a39b8936d72ad0d5f470f2de919d4e
Nancy Harris <nanzyharris@gmail.com>
2019-11-09 19:51:08 -0600

    changes to javascript file and style css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,28 @@
 // GLOBAL VARIABLES
 // ========================================================================
 // Arrays and variables for holding data
-
+// This variable contains an array with the possible hidden words:
 var wordOptions = ["mustang", "continental", "marker", "daisy", "parabellum", "bourbon", "boogeyman", "jonathan", "excommunicado", "jardani", "jovanovich", "helen"];
+// This variable contains the randomly selected word that the user has to guess
 var selectedWord = "";
+// This variable holds an array which contains the letters that compose the selected word to be guessed 
 var lettersinWord = [];
+// This variable holds the number of blanks or number of spaces for the letters that form the hidden word
 var numBlanks = 0;
+// This variable holds an array with the letters that the user will type which either will be right or wrong
 var blanksAndSuccesses = [];
+// This variable holds an array which will contain the letters the user typed but are a wrong guess
 var wrongLetters = [];
 
 // Game counters
-
+// Variable for counter for the games won
 var winCount = 0;
+// Variable for counter for the games lost
 var lossCount = 0;
+// Variable for counter for the attempts left
 var guessesLeft = 0;
-var wrongLtr = 0;
+// Variable for counter for the letters that the user typed but which were a wrong guess
+var wrongLtr = "&nbsp";
 
 // FUNCTIONS (reusable blocks of code that I will call upon when needed)
 // ========================================================================
@@ -62,7 +70,7 @@ function checkLetters(letter) {
             isLetterInWord = true;
         }
     }
-    //Check where in word the letter exists
+    //Check or locate if the letter exists in the current word
     for (var i = 0; i < numBlanks; i++) {
         if (selectedWord[i] == letter) {
             blanksAndSuccesses[i] = letter;
@@ -87,7 +95,11 @@ function roundComplete() {
     document.getElementById("numGuesses").innerHTML = guessesLeft;
     document.getElementById("wrongGuess").innerHTML = wrongLtr;
     document.getElementById("winCounter").innerHTML = winCount;
-    document.getElementById("wrongGuess").innerHTML = wrongLetters.join(" ");
+    // Adding an If statement to keep the 'Wrong Guesses' cointainer on the webpage from shrinking before it is populated
+    console.log(wrongLetters, " letters")
+    if (wrongLetters.length >= 1) {
+        document.getElementById("wrongGuess").innerHTML = wrongLetters.join(" ");
+    }
 
     // Check if user won
     if (lettersinWord.toString() == blanksAndSuccesses.toString()) {
@@ -125,7 +137,7 @@ function roundComplete() {
 window.onload = function () {
     alert("Just type a letter to start the game, and away we go...");
     startGame();
-    checkLetters();
+    // checkLetters();
     roundComplete();
     document.getElementById("wrongGuess").innerHTML = wrongLtr;
     document.onkeyup = function (event) {
